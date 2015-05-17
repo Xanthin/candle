b77b3813520744a5d65c1a495457058d7b5edb3c
Xanthin <Xanthin@users.noreply.github.com>
2015-05-17 02:59:18 +0200

    finaly the update with textures

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ end
 --The Blocks
 
 minetest.register_node("candle:basiccandle", {
-	drop = 'candle:basiccandle',
 	description = S("Candle"),
 	drawtype = "plantlike",
 	sunlight_propogates=true,
@@ -22,7 +21,6 @@ minetest.register_node("candle:basiccandle", {
 })
 
 minetest.register_node("candle:teacandle", {
-	drop = 'candle:teacandle',
 	description = S("Tea Candle"),
 	drawtype = "plantlike",
 	sunlight_propogates=true,
@@ -33,7 +31,6 @@ minetest.register_node("candle:teacandle", {
 })
 
 minetest.register_node("candle:fatcandle", {
-	drop = 'candle:fatcandle',
 	description = S("Fat Candle"),
 	drawtype = "plantlike",
 	sunlight_propogates=true,
@@ -44,7 +41,6 @@ minetest.register_node("candle:fatcandle", {
 })
 
 minetest.register_node("candle:chandelier", {
-	drop = 'candle:chandelier',
 	description = S("Chandelier"),
 	drawtype = "plantlike",
 	sunlight_propogates=true,
@@ -54,8 +50,58 @@ minetest.register_node("candle:chandelier", {
 	light_source=14
 })
 
+-- new items
+minetest.register_node("candle:chandelier_gold", {
+	description = S("Chandelier Gold"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"chandelier_gold.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:chandelier_silver", {
+	description = S("Chandelier Silver"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"chandelier_silver.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:chandelier_mithril", {
+	description = S("Chandelier Mithril"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"chandelier_mithril.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:chandelier_bronze", {
+	description = S("Chandelier Bronze"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"chandelier_bronze.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:chandelier_iron", {
+	description = S("Chandelier Iron"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"chandelier_iron.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
 minetest.register_node("candle:waxystuff", {
-	drop = 'candle:waxystuff',
 	description = S("Waxy Stuff"),
 	drawtype = "raillike",
 	sunlight_propogates=true,
@@ -65,7 +111,6 @@ minetest.register_node("candle:waxystuff", {
 })
 
 minetest.register_node("candle:candelabra", {
-	drop = 'candle:candelabra',
 	description = S("Candelabra"),
 	drawtype = "plantlike",
 	sunlight_propogates=true,
@@ -75,15 +120,75 @@ minetest.register_node("candle:candelabra", {
 	light_source=14
 })
 
+-- neues Item nach Vorlage
+minetest.register_node("candle:candelabra_gold", {
+	description = S("Candelabra Gold"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"candelabra_gold.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:candelabra_silver", {
+	description = S("Candelabra Silver"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"candelabra_silver.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:candelabra_mithril", {
+	description = S("Candelabra Mithril"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"candelabra_mithril.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:candelabra_bronze", {
+	description = S("Candelabra Bronze"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"candelabra_bronze.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:candelabra_iron", {
+	description = S("Candelabra Iron"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"candelabra_iron.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
+
+minetest.register_node("candle:candelabra_glass", {
+	description = S("Candelabra Glass"),
+	drawtype = "plantlike",
+	sunlight_propogates=true,
+	tiles = {"candelabra_glass.png"},
+	walkable=false,
+	groups={level=1,oddly_breakable_by_hand=1},
+	light_source=14
+})
 
 --The Crafting Recipes
 
 minetest.register_craft({
 	output = "candle:basiccandle 2",
 	recipe = {
-		{'', 'default:papyrus', ''},
-		{'candle:waxystuff', 'default:papyrus', 'candle:waxystuff'},
-		{'candle:waxystuff', 'default:papyrus', 'candle:waxystuff'},
+		{'', 'farming:string', ''},
+		{'candle:waxystuff', 'farming:string', 'candle:waxystuff'},
+		{'candle:waxystuff', 'farming:string', 'candle:waxystuff'},
 	}
 })
 
@@ -91,7 +196,7 @@ minetest.register_craft({
 	output = "candle:teacandle 2",
 	recipe = {
 		{'', '', ''},
-		{'', 'default:papyrus', ''},
+		{'', 'farming:string', ''},
 		{'candle:waxystuff', 'candle:waxystuff', 'candle:waxystuff'},
 	}
 })
@@ -99,7 +204,7 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "candle:fatcandle 2",
 	recipe = {
-		{'', 'default:papyrus', ''},
+		{'', 'farming:string', ''},
 		{'candle:waxystuff', 'candle:waxystuff', 'candle:waxystuff'},
 		{'candle:waxystuff', 'candle:waxystuff', 'candle:waxystuff'},
 	}
@@ -111,6 +216,52 @@ minetest.register_craft({
 		{'', 'default:steel_ingot', ''},
 		{'candle:basiccandle', 'default:steel_ingot', 'candle:basiccandle'},
 		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+	}
+})
+
+--neues Rezept nach Vorlage
+minetest.register_craft({
+	output = "candle:chandelier_gold 1",
+	recipe = {
+		{'', 'default:gold_ingot', ''},
+		{'candle:basiccandle', 'default:gold_ingot', 'candle:basiccandle'},
+		{'default:gold_ingot', 'default:gold_ingot', 'default:gold_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:chandelier_silver 1",
+	recipe = {
+		{'', 'moreores:silver_ingot', ''},
+		{'candle:basiccandle', 'moreores:silver_ingot', 'candle:basiccandle'},
+		{'moreores:silver_ingot', 'moreores:silver_ingot', 'moreores:silver_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:chandelier_mithril 1",
+	recipe = {
+		{'', 'moreores:mithril_ingot', ''},
+		{'candle:basiccandle', 'moreores:mithril_ingot', 'candle:basiccandle'},
+		{'moreores:mithril_ingot', 'moreores:mithril_ingot', 'moreores:mithril_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:chandelier_bronze 1",
+	recipe = {
+		{'', 'default:bronze_ingot', ''},
+		{'candle:basiccandle', 'default:bronze_ingot', 'candle:basiccandle'},
+		{'default:bronze_ingot', 'default:bronze_ingot', 'default:bronze_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:chandelier_iron 1",
+	recipe = {
+		{'', 'default:iron_lump', ''},
+		{'candle:basiccandle', 'default:iron_lump', 'candle:basiccandle'},
+		{'default:iron_lump', 'default:iron_lump', 'default:iron_lump'},
 	}
 })
 
@@ -129,5 +280,60 @@ minetest.register_craft({
 		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
 		{'', 'default:steel_ingot', ''},
 		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+	}
+})
+
+-- recipes for the new items
+minetest.register_craft({
+	output = "candle:candelabra_gold 1",
+	recipe = {
+		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
+		{'', 'default:gold_ingot', ''},
+		{'default:gold_ingot', 'default:gold_ingot', 'default:gold_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:candelabra_silver 1",
+	recipe = {
+		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
+		{'', 'moreores:silver_ingot', ''},
+		{'moreores:silver_ingot', 'moreores:silver_ingot', 'moreores:silver_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:candelabra_mithril 1",
+	recipe = {
+		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
+		{'', 'moreores:mithril_ingot', ''},
+		{'moreores:mithril_ingot', 'moreores:mithril_ingot', 'moreores:mithril_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:candelabra_bronze 1",
+	recipe = {
+		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
+		{'', 'default:bronze_ingot', ''},
+		{'default:bronze_ingot', 'default:bronze_ingot', 'default:bronze_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:candelabra_iron 1",
+	recipe = {
+		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
+		{'', 'default:iron_lump', ''},
+		{'default:iron_lump', 'default:iron_lump', 'default:iron_lump'},
+	}
+})
+
+minetest.register_craft({
+	output = "candle:candelabra_glass 1",
+	recipe = {
+		{'candle:basiccandle', 'candle:basiccandle', 'candle:basiccandle'},
+		{'', 'default:glass', ''},
+		{'default:glass', 'default:glass', 'default:glass'},
 	}
 })
